ed677a2fbba5e12f076c479bec9c7a7d1842faa5
malinfischer <malin.fischer@web.de>
2019-04-28 01:48:16 +0200

    8

---
+++ R/example_code.R
@@ -0,0 +1,16 @@
+################################################
+###### R-Package phenoTS - examplary code ######
+################################################
+
+### install and activate package
+
+# devtools package
+install.packages("devtools")
+library(devtools)
+
+# actual phenoTS package from github
+devtools::install_github("malinfischer/phenoTS")
+library(phenoTS)
+
+
+
